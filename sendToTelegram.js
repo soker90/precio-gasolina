436bc632abcd9927d4b95f7840364d8c163d8632
Eduardo Parra <edue17@gmail.com>
2022-02-26 12:40:50 +0100

    fix

--- sendToTelegram.js
+++ sendToTelegram.js
@@ -8,26 +8,30 @@ const DIESEL_CHAT_ID = process.env.DIESEL_CHAT_ID;
 
 const sendToTelegram = ({price, priceOld, type, chatId}) => {
   if(price !== priceOld){
-    const msg = `El precio ${type} ha ${price > 0 ? subido : bajado} ${Math.abs(price - priceOld)}`
+    const diff = Math.round(Math.abs(price - priceOld) * 1000 ) / 1000
+    const msg = `*${date}*: El precio ${type} ha _${price > 0 ? 'subido' : 'bajado'}_ *${diff}*`
     console.log(msg)
-    bot.sendMessage(chatId, msg);
+    bot.sendMessage(chatId, msg, {parse_mode : 'Markdown'});
   }
 
 }
 
 const dataSaved = readFile()
+const date = dataSaved.dates.pop()
 
 sendToTelegram({
   price: dataSaved.gasolina.pop(),
   priceOld: dataSaved.gasolina.pop(),
   type: 'de la gasolina',
-  chatId: GASOLINA_CHAT_ID
+  chatId: GASOLINA_CHAT_ID,
+  date
 })
 
 sendToTelegram({
   price: dataSaved.diesel.pop(),
   priceOld: dataSaved.diesel.pop(),
   type: 'del diesel',
-  chatId: DIESEL_CHAT_ID
+  chatId: DIESEL_CHAT_ID,
+  date
 })
   
