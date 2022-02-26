96606e15a2243e3544755ca53c36f0c4f15fc901
Eduardo Parra <edue17@gmail.com>
2022-02-26 13:44:46 +0100

    improvements

--- download.mjs
+++ download.mjs
@@ -11,11 +11,17 @@ fetch(GEOPORTAL_URL, {headers: { 'Accept': ' application/json' }})
 .then((stationData) => {
     let dataSaved = readFile()
     
-    dataSaved.dates.push(date)
-    dataSaved.gasolina.push(stationData.precioGasolina95E5)
-    dataSaved.diesel.push(stationData.precioGasoleoA)
-  
-    writeFile(dataSaved);
+    if(dataSaved.dates.at(-1) === date){
+      const numElments = dataSaved.dates.length - 1
+      dataSaved.gasolina[numElments] = stationData.precioGasolina95E5
+      dataSaved.diesel[numElments] = stationData.precioGasoleoA
+    } else {
+      dataSaved.dates.push(date)
+      dataSaved.gasolina.push(stationData.precioGasolina95E5)
+      dataSaved.diesel.push(stationData.precioGasoleoA)
+    }
+    
+    writeFile(dataSaved)
 
     console.log(`Guardado con fecha ${date}: gasolina: ${stationData.precioGasolina95E5} y gasoil: ${stationData.precioGasoleoA}`)
     
