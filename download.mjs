c7939a64ebd3e73b54741de50c52797646229f3d
aserrno <alejandroserranonotivoli@gmail.com>
2023-08-11 11:44:49 +0200

    change source url and changes logic

--- download.mjs
+++ download.mjs
@@ -1,29 +1,39 @@
 import fetch from 'node-fetch'
 import { readFile, writeFile } from './fileUtils.js'
+import pkg from 'dotenv';
+const { dotenv } = pkg;
+pkg.config();
 
-const FUEL_STATION_ID = process.env.FUEL_STATION_ID
-const GEOPORTAL_URL = `https://geoportalgasolineras.es/rest/${FUEL_STATION_ID}/busquedaEstacionPrecio`
+const IDMUNICIPIO = process.env.IDMUNICIPIO;
+const IDPRODUCTO = process.env.IDPRODUCTO;
+const GEOPORTAL_URL = `https://sedeaplicaciones.minetur.gob.es/ServiciosRESTCarburantes/PreciosCarburantes/EstacionesTerrestres/FiltroMunicipio/${IDMUNICIPIO}`
 
 const date = new Intl.DateTimeFormat('es-ES', { month: 'numeric', day: 'numeric' }).format(Date.now())
 
 fetch(GEOPORTAL_URL, {headers: { 'Accept': ' application/json' }})
 .then(res => res.json())
 .then((stationData) => {
-    let dataSaved = readFile()
+    let dataSaved = readFile();
+    console.log(dataSaved);
+
+    const priceGasolina95 = Number(stationData['ListaEESSPrecio'][0]['Precio Gasolina 95 E5'].replace(',','.'));
+    const priceGasoleoA = Number(stationData['ListaEESSPrecio'][0]['Precio Gasoleo A'].replace(',','.'));
     
     if(dataSaved.dates.at(-1) === date){
       const numElments = dataSaved.dates.length - 1
-      dataSaved.gasolina[numElments] = stationData.precioGasolina95E5
-      dataSaved.diesel[numElments] = stationData.precioGasoleoA
+      dataSaved.gasolina[numElments] = priceGasolina95
+      dataSaved.diesel[numElments] = priceGasoleoA
     } else {
       dataSaved.dates.push(date)
-      dataSaved.gasolina.push(stationData.precioGasolina95E5)
-      dataSaved.diesel.push(stationData.precioGasoleoA)
+      dataSaved.gasolina.push(priceGasolina95)
+      dataSaved.diesel.push(priceGasoleoA)
     }
     
     writeFile(dataSaved)
 
-    console.log(`Guardado con fecha ${date}: gasolina: ${stationData.precioGasolina95E5} y gasoil: ${stationData.precioGasoleoA}`)
+    // console.log(dataSaved)
+
+    console.log(`Fecha ${date} => Gasolina 95: ${priceGasolina95} - Gasoleo A: ${priceGasoleoA}`)
     
 })
   .catch(err => {
